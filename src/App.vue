648676f5c922f268486f37a854a728e9a74481af
CN087005 <jay.liang@amway.com>
2023-12-04 01:55:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="{{CONTAINER}}">
+    <MyVueJSButton />
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import MyVueJSButton from './MyVueJSButton.vue';
+  export default defineComponent({
+    components:{
+      MyVueJSButton
+    }
+  });
+</script>
---
+++ src/MyVueJSButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>Provided by RemoteVueJs Project - {MyVueJSButton.vue}</div>
+    <p>{{ num }}</p>
+    <button @click="increment">click me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'MyVueJSButton',
+  setup() {
+    const num = ref(0);
+
+    const increment = () => {
+      num.value += 2;
+    };
+
+    return { num, increment };
+  }
+};
+</script>
+
+<style>
+
+</style>
